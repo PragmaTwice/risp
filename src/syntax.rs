a99b2d3c1c3c508736a5547923a942034873f505
PragmaTwice <e.d.boy@aliyun.com>
2018-05-17 15:19:22 +0800

    add operators

--- src/syntax.rs
+++ src/syntax.rs
@@ -3,18 +3,65 @@ trace_macros!(false);
 macro_rules! risp_unit_checker {
     ($unit:literal) => ($unit);
     ($unit:ident) => ($unit);
+    (_) => (_);
 }
 
-macro_rules! risp_item_transfer {
-    {(define $id:ident $v:tt)} => { let $id = risp!{$v}; };
-    {(begin $($item:tt)*)} => {{$(risp!{$item});*}};
+macro_rules! risp_item_transfer {    
+    {(define $p:tt $v:tt)} => { let risp!($p) = risp!{$v}; };
+
+    {(tuple $($v:tt)+)} => { ($(risp!($v),)+) };
+
+    {(mut $p:tt)} => { mut $p };
+
+    {(begin $($item:tt)+)} => {{ $(risp!{$item});+ }};
     {(lambda ($($para:tt)*) $item:tt)} => {(|$($para),*|risp!{$item})};
     
     {(if $cond:tt $t:tt)} => {if risp!{$cond} {risp!($t)}};
     {(if $cond:tt $t:tt $f:tt)} => {if risp!{$cond} {risp!($t)} else {risp!($f)}};
+    {(match $v:tt $((=> $p:tt $q:tt))*)} => {match risp!{$v} {$(risp!{$p} => risp!{$q}),*}};
     
     {(loop $l:tt)} => {loop {risp!($l)}};
     {(while $cond:tt $l:tt)} => {while risp!{$cond} {risp!($l)}};
+    { break } => { break };
+    { continue } => { continue };
+    
+    {(+ $v1:tt $($vn:tt)+)} => {risp!($v1) $(+ risp!{$vn})+};
+    {(- $v1:tt)} => {- risp!($v1)};
+    {(- $v1:tt $v2:tt)} => {risp!($v1) - risp!{$v2}};
+    {(* $v1:tt $($vn:tt)+)} => {risp!($v1) $(* risp!{$vn})+};
+    {(/ $v1:tt $v2:tt)} => {risp!($v1) / risp!{$v2}};
+    {(% $v1:tt $v2:tt)} => {risp!($v1) % risp!{$v2}};
+
+    {(& $v1:tt $($vn:tt)+)} => {risp!($v1) $(& risp!{$vn})+};
+    {(| $v1:tt $($vn:tt)+)} => {risp!($v1) $(| risp!{$vn})+};
+    {(^ $v1:tt $($vn:tt)+)} => {risp!($v1) $(^ risp!{$vn})+};
+    {(<< $v1:tt $v2:tt)} => {risp!($v1) << risp!{$v2}};
+    {(>> $v1:tt $v2:tt)} => {risp!($v1) >> risp!{$v2}};
+    
+    {(= $v1:tt $v2:tt)} => {risp!($v1) = risp!{$v2}};
+    {(+= $v1:tt $v2:tt)} => {risp!($v1) += risp!{$v2}};
+    {(-= $v1:tt $v2:tt)} => {risp!($v1) -= risp!{$v2}};
+    {(*= $v1:tt $v2:tt)} => {risp!($v1) *= risp!{$v2}};
+    {(/= $v1:tt $v2:tt)} => {risp!($v1) /= risp!{$v2}};
+    {(%= $v1:tt $v2:tt)} => {risp!($v1) %= risp!{$v2}};
+    {(&= $v1:tt $v2:tt)} => {risp!($v1) &= risp!{$v2}};
+    {(|= $v1:tt $v2:tt)} => {risp!($v1) |= risp!{$v2}};
+    {(^= $v1:tt $v2:tt)} => {risp!($v1) ^= risp!{$v2}};
+    {(<<= $v1:tt $v2:tt)} => {risp!($v1) <<= risp!{$v2}};
+    {(>>= $v1:tt $v2:tt)} => {risp!($v1) >>= risp!{$v2}};
+
+    {(== $v1:tt $v2:tt)} => {risp!($v1) == risp!{$v2}};
+    {(!= $v1:tt $v2:tt)} => {risp!($v1) != risp!{$v2}};
+    {(< $v1:tt $v2:tt)} => {risp!($v1) < risp!{$v2}};
+    {(<= $v1:tt $v2:tt)} => {risp!($v1) <= risp!{$v2}};
+    {(> $v1:tt $v2:tt)} => {risp!($v1) > risp!{$v2}};
+    {(>= $v1:tt $v2:tt)} => {risp!($v1) >= risp!{$v2}};
+
+    {(! $v1:tt)} => {! risp!($v1)};
+    {(&& $v1:tt $($vn:tt)+)} => {risp!($v1) $(&& risp!{$vn})+};
+    {(|| $v1:tt $($vn:tt)+)} => {risp!($v1) $(|| risp!{$vn})+};
+
+    {(as $v1:tt $v2:tt)} => {risp!($v1) as risp!{$v2}};
 
     {($fn:tt $($item:tt)*)} => {risp!($fn)($(risp!{$item}),*)};
 }
